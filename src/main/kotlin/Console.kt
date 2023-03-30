bea797cf47b3c0357b7ea54573857c1ca1354c66
CharlesJKK <selrahcjunior@hotmail.com>
2023-03-30 02:05:05 -0300

    feat: classes de console e portaria já orientada a objetos

--- src/main/kotlin/Console.kt
+++ src/main/kotlin/Console.kt
@@ -1,2 +1,47 @@
-class Console {
+class Console private constructor(){
+
+    companion object{
+        fun readInt (msg: String): Int{
+
+            var response: Int? = null
+
+            do {
+                print(msg)
+                val info = readLine()
+                if(info != null && info != ""){
+
+                    response = info.toIntOrNull()
+
+                }else{
+
+                    if(response == null || response <= 0){
+                        println("Valor inválido")
+                    }
+
+                }
+            }while (response == null || response <= 0)
+
+            return response
+        }
+
+        fun readString(msg: String): String{
+
+            var response: String? = null
+
+            do {
+                print(msg)
+                val info = readLine()
+
+                if (info != null && info != ""){
+                    response = info.lowercase()
+                }else{
+                    println("Valor inválido")
+                }
+
+            }while (response == null)
+
+            return response
+        }
+    }
+
 }
